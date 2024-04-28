ff6dd82ff1778fdf1bf10ee76155ec6f2b948e0c
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-28 11:20:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let output_div = document.querySelector("#output")
+const input_text = document.querySelector("#text")
+const input_delay = document.querySelector("#delay")
+const btn = document.querySelector("#btn")
+
+btn.addEventListener("click",async()=>{
+	await new Promise(resolve => {
+		setTimeout(()=>{
+			resolve()
+		},input_delay.value)
+	})
+	output_div.innerText = `${input_text.value}`
+})
